69d3e8381fafa4fcb981b387daa0bbf5f2a6a647
unknown <eddison>
2022-07-17 10:09:30 -0500

    retoFinal

---
+++ src/data/Data.js
@@ -0,0 +1,54 @@
+export const tv = {
+  topLine: "TV",
+  headLine: "Disfruta en tu TV.",
+  descripcion:
+    "Ve en smart TV, PlayStation, Xbox, Chromecast, Apple TV, reproductores de Blu-ray y más.",
+  buttonLabel: "Comenzar",
+  img: "images/img_tv.svg",
+  alt: "tv",
+  imgStart: "start",
+  lighBackgroud: true,
+  lighHeadText: false,
+  lighDescription: false,
+};
+
+export const series = {
+  topLine: "SERIES",
+  headLine: "Descarga tus series para verlas offline.",
+  descripcion: "Guarda tu contenido favorito y tendrás siempre algo para ver.",
+  buttonLabel: "Comenzar",
+  img: "images/img_tv.svg",
+  alt: "series",
+  imgStart: "",
+  lighBackgroud: false,
+  lighHeadText: true,
+  lighDescription: true,
+};
+
+export const peliculas = {
+  topLine: "Peliculas",
+  headLine: "Disfruta donde quieras.",
+  descripcion:
+    "Películas y series ilimitadas en tu teléfono, tablet, computadora y TV sin costo extra.",
+  buttonLabel: "Comenzar",
+  img: "images/img_tv.svg",
+  alt: "peliculas",
+  imgStart: "start",
+  lighBackgroud: true,
+  lighHeadText: false,
+  lighDescription: false,
+};
+
+export const infantil = {
+  topLine: "INFANTIL",
+  headLine: "Crea perfiles para niños.",
+  descripcion:
+    "Los niños vivirán aventuras con sus personajes favoritos en un espacio diseñado exclusivamente para ellos, sin costo con tu membresía.",
+  buttonLabel: "Comenzar",
+  img: "images/img_tv.svg",
+  alt: "infantil",
+  imgStart: "",
+  lighBackgroud: false,
+  lighHeadText: true,
+  lighDescription: true,
+};
